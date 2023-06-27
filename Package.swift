5f157b923fae7752f5838f880c4f75e28e51bf6e
David Ben-Yaakov <david@codermerlin.academy>
2023-06-27 18:44:14 -0500

    Updated to latest dependency versions
    * Updated to Swift-Crypto 2.5.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/apple/swift-crypto.git", .exact("2.0.5"))
+    .package(url: "https://github.com/apple/swift-crypto.git", .exact("2.5.0"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
